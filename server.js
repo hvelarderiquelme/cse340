38d52f83a07ab37a9497726bc79003a2029cad51
hvelarderiquelme <hvelardeab@gmail.com>
2026-05-28 13:54:19 -0600

    main page not working

--- server.js
+++ server.js
@@ -69,6 +69,7 @@ app.use((req, res, next) => {
     next(err);
 });
 
+
 // Global error handler
 app.use((err, req, res, next) => {
     // Log error details for debugging
--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,5 +1,6 @@
 const homePage = (req, res) => {
     const title = 'Home';
+    console.log('HOME PAGE HIT');
     res.render('home', { title });
 };
 
